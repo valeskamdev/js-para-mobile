61f5857f65729e76d9671c0566d9c2e6dac44a7e
Valeska Marques <valeskkitty@gmail.com>
2023-11-22 08:53:53 -0300

    feat: concatenação tradicional

---
+++ 03-template-literal.js
@@ -0,0 +1,15 @@
+const nome = "Agatha";
+const sobrenome = "Marques";
+
+let idade = 16;
+let cidade = "São Paulo";
+let estado = "SP";
+
+/*
+* Concatenação tradicional usando operador + Meu nome é Agatha Giovanna,
+*  tenho 16 anos e moro atualmente na cidade de São Paulo - SP.  */
+
+let mensagem1 = "Meu nome é " + nome + " " + sobrenome + ", tenho "
+  + idade + " anos e moro atualmente na cidade de " + cidade + " - " + estado + ".";
+
+console.log(mensagem1);
