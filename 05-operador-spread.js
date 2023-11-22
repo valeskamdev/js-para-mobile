9128cb545e84eb5f235dd3900e88d3c3f4e4ebf1
Valeska Marques <valeskkitty@gmail.com>
2023-11-22 11:54:03 -0300

    feat: operador spread em objetos

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -9,3 +9,21 @@ const maisBandas = [...bandaProg, "Slayer", "Nightwish"]; // Copia os elementos 
 
 console.log(bandaProg);
 console.log(maisBandas); // ["Pink Floyd", "Rush", "Yes", "Slayer", "Nightwish"]
+
+// Spread com objetos
+
+const cliente = {
+  nome: "Seu Madruga",
+  cidade: "São Paulo",
+  idade: 55
+}
+
+const novosDados = {
+  ...cliente, // Copia os dados do objeto cliente para o objeto novosDados
+  estado: "SP",
+  pedido: "123xyz",
+  total: 5000.88,
+  idade: 34 // Sobrescreve o valor da propriedade idade do objeto cliente
+}
+
+console.log(novosDados);
