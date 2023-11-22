a4722c8ebffbc823dc4ebb64b150c78648133ff2
Valeska Marques <valeskkitty@gmail.com>
2023-11-22 09:08:31 -0300

    feat: concatenação template string

--- 03-template-literal.js
+++ 03-template-literal.js
@@ -9,7 +9,18 @@ let estado = "SP";
 * Concatenação tradicional usando operador + Meu nome é Agatha Giovanna,
 *  tenho 16 anos e moro atualmente na cidade de São Paulo - SP.  */
 
+/*
+  Concatenação tradicional usando operador + Meu nome é Agatha Giovanna,
+  tenho 16 anos e moro atualmente na cidade de São Paulo - SP.
+*/
+
 let mensagem1 = "Meu nome é " + nome + " " + sobrenome + ", tenho "
   + idade + " anos e moro atualmente na cidade de " + cidade + " - " + estado + ".";
 
 console.log(mensagem1);
+
+/* Template Literal/String */
+
+let mensagem2 = `Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e moro atualmente na cidade de ${cidade} - ${estado}. Sou ${idade >= 18 ? "maior" : "menor"} de idade.`;
+
+console.log(mensagem2);
