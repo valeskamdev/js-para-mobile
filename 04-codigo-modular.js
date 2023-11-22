cac25a893267c186d50e25b533327bc69925b6c4
Valeska Marques <valeskkitty@gmail.com>
2023-11-22 09:39:25 -0300

    feat: exportando vários recursos

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,4 +1,4 @@
-import pessoa from './modules/dados.js';
+import { pessoa, livro } from './modules/dados.js';
 
 console.log(pessoa);
 console.log(pessoa.nome);
@@ -7,4 +7,13 @@ console.log("\n");
 
 for (const pessoaKey in pessoa) {
   console.log(pessoa[pessoaKey]);
+}
+
+console.log("\n");
+
+console.log(livro);
+console.log(livro.volume);
+
+for (const livroKey in livro) {
+  console.log(livro[livroKey]);
 }
--- modules/dados.js
+++ modules/dados.js
@@ -4,4 +4,18 @@ const pessoa = {
   cidade: 'São Paulo'
 }
 
-export default pessoa;
+const livro = {
+  titulo: "O Senhor dos Anéis",
+  volume: "As Duas Torres",
+  ano: 1954
+}
+
+/*
+  Se for exportação de somente UM recurso usamoas export default nomeDoRecurso
+*/
+// export default pessoa;
+
+/*
+  Se for exportação de VÁRIOS recursos usamoas somente o export { entre chaves os recursos }
+*/
+export { pessoa, livro };
