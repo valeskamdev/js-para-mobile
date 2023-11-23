9c9e6ae5aac101f5512db176fe77d3bb1e499a6b
Valeska Marques <valeskkitty@gmail.com>
2023-11-23 08:46:28 -0300

    refactor: operador rest

--- 06-operador-rest.js
+++ 06-operador-rest.js
@@ -9,9 +9,7 @@ const cientistas = ["Tesla", "Einstein", "Newton", "Darwin"];
 const artistas = ["Da Vinci", "Van Gogh", "Mozart", "Picasso", "Gauguin", "Matisse"];
 
 // Usando o rest operator (...parametros) podemos mesclar uma lista de parâmetros/argumentos para a função
-const classificar = (...parametros) => {
-    return parametros.sort();
-}
+const classificar = (...parametros) => parametros.sort();
 
 console.log(classificar(...cientistas)); // spread (espalha os elementos do array)
 console.log(classificar(...artistas)); // spread
