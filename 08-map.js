a26df7347007f8c24a21bf76c11639500fd970a4
Valeska Marques <valeskkitty@gmail.com>
2023-11-23 10:58:30 -0300

    feat: map de títulos

--- 08-map.js
+++ 08-map.js
@@ -1,9 +1,10 @@
+import cursos from './modules/cursos.js';
 
 /*
   map (mapear, mapeia - transformar em outra coisa)
 
   Passa po elementos de um array e realiza operações e, cada um
-  deles atráves de uma função, gerando um novo array
+  deles atráves de uma função (callback), gerando um novo array
  */
 
 // Exemplo 1
@@ -12,3 +13,11 @@ const numeros = [10, 20, 5, 12, 66, 50];
 const numerosDobrados = numeros.map(num => num * 2);
 
 console.log(numerosDobrados);
+console.log("\n");
+
+console.log(cursos);
+
+// Exemplo 2: gerar um novo array apenas com os nomes dos cursos
+const titulos = cursos.map(nome => nome.titulo);
+
+console.log(titulos);
