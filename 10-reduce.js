7b95e38ebdc5eccb6a4cba674db74d430c970a24
Valeska Marques <valeskkitty@gmail.com>
2023-11-24 11:11:07 -0300

    feat: exemplos sem reduce

--- 10-reduce.js
+++ 10-reduce.js
@@ -8,9 +8,28 @@
 
 // Exemplo 1
 const valores = [10, 5, 50, 200, 1000];
+
 const total = valores.reduce((acumulador, valor) => {
   return acumulador + valor;
 }, 0);
 
 console.log(valores);
-console.log(total);
+console.log(total); // 1265
+
+// Sem reduce com for
+let acumuladora1 = 0;
+
+for (let i = 0; i < valores.length; i++) {
+  acumuladora1 += valores[i];
+}
+
+console.log(acumuladora1); // 1265
+
+// Sem reduce com for/of
+let acumuladora2 = 0;
+
+for (const valor of valores) {
+  acumuladora2 += valor;
+}
+
+console.log(acumuladora2); // 1265
