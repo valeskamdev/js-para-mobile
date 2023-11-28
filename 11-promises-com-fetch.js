e11b7763e99ba9d4ef1b981d175df2c72909eeab
Valeska Marques <valeskkitty@gmail.com>
2023-11-28 09:11:14 -0300

    feat: validação OK da resposta

--- 11-promises-com-fetch.js
+++ 11-promises-com-fetch.js
@@ -4,12 +4,23 @@
   usando fetch/then/catch
  */
 
-const api_url = `https://jsonplaceholder.typicode.com/users/3`;
+const api_url = `https://jsonplaceholder.typicode.com/users/12`;
 
 // Conecte/Acesse..
 fetch(api_url)
   // ... e então captura resposta da API no formato JSON
-  .then(response => response.json())
+  .then(response => {
+    /*
+    Se a resposta da requisição não for bem-sucedida ( por exemplo,
+    se não hoouver registros, ou se der erro no próprio server [500]
+     */
+    if (!response.ok) {
+      throw new Error(
+        `Erro na requisição: ${response.status} - ${response.statusText}`
+      )
+    }
+    return response.json();
+  })
   //... e então capture os dados contidos na resposta
   .then(json => console.log(json))
   // Em caso de erro (acesso, no json, gerais) capture o erro
